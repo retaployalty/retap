98ba181863c4f11b4cbd108c315e33ab69cb0788
semcripto <127352471+semcripto@users.noreply.github.com>
2025-06-26 15:45:03 +0200

    WIP: modifiche locali prima del merge

--- apps/website/public/webapp/flutter_bootstrap.js
+++ apps/website/public/webapp/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "589165684"
+    serviceWorkerVersion: "4010908132"
   }
 });
